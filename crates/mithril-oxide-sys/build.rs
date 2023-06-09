3c412808dfdc322b7f9f8aae6c99592d49d800a2
Edgar Luque <git@edgarluque.com>
2023-06-09 14:05:37 +0200

    link to llvm if shared

--- crates/mithril-oxide-sys/build.rs
+++ crates/mithril-oxide-sys/build.rs
@@ -1,12 +1,32 @@
-use std::env::var;
+use std::{
+    env::var,
+    path::Path,
+    process::{Command, Stdio}, io::Read,
+};
 
 fn main() {
-    println!(
-        "cargo:rustc-link-search={}/lib",
-        env!("MLIR_SYS_160_PREFIX")
-    );
+    let mlir_env = env!("MLIR_SYS_160_PREFIX");
+    println!("cargo:rustc-link-search={}/lib", mlir_env);
     println!("cargo:rustc-link-search={}", var("OUT_DIR").unwrap());
     // linking to llvm here works if llvm-config --shared outputs shared, not static, because in static mlir likely includes llvm too.
-    // println!("cargo:rustc-link-lib=LLVM");
+
+    let mut process = Command::new(dbg!(Path::new(mlir_env).join("bin/llvm-config")))
+        .arg("--shared-mode")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .unwrap();
+    process.wait().unwrap();
+
+    let mut output = String::new();
+    process.stdout.unwrap().read_to_string(&mut output).unwrap();
+    let output = output.trim();
+
+    match output {
+        "static" => {}
+        "shared" => println!("cargo:rustc-link-lib=LLVM"),
+        _ => panic!("unknown shared mode: {}", output)
+    }
     println!("cargo:rustc-link-lib=MLIR");
 }
