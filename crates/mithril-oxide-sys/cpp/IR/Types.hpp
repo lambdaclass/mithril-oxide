2dd1971e1de10bcb28e72703e09f1f3f849d3de0
Edgar Luque <git@edgarluque.com>
2023-06-12 19:40:35 +0200

    types

---
+++ crates/mithril-oxide-sys/cpp/IR/Types.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <memory>
+
+#include <mlir/IR/Types.h>
+
+
+namespace mithril_oxide_sys {
+
+
+} // namespace mithril_oxide_sys
--- crates/mithril-oxide-sys/src/IR.rs
+++ crates/mithril-oxide-sys/src/IR.rs
@@ -3,3 +3,4 @@ pub mod BuiltinOps;
 pub mod DialectRegistry;
 pub mod Location;
 pub mod MLIRContext;
+pub mod Types;
