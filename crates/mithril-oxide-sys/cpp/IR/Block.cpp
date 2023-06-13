43b66653a9ccfc43ca2003cd1aa109edae12bfc9
Edgar <git@edgarluque.com>
2023-06-13 17:37:02 +0200

    conflict

--- crates/mithril-oxide-sys/cpp/IR/Block.cpp
+++ crates/mithril-oxide-sys/cpp/IR/Block.cpp
@@ -1,4 +1,5 @@
 #include "Block.hpp"
+#include <memory>
 
 
 namespace mithril_oxide_sys {
@@ -6,6 +7,11 @@ namespace mithril_oxide_sys {
 void Block_addArgument(Block &block, const Type& type, const Location& loc)
 {
     block.addArgument(type, loc);
+}
+
+std::unique_ptr<BlockArgument> Block_getArgument(Block &block, unsigned i)
+{
+    return std::make_unique<BlockArgument>(block.getArgument(i));
 }
 
 } // namespace mithril_oxide_sys
--- crates/mithril-oxide-sys/cpp/IR/Block.hpp
+++ crates/mithril-oxide-sys/cpp/IR/Block.hpp
@@ -13,8 +13,10 @@ namespace mithril_oxide_sys {
 using mlir::Block;
 using mlir::Type;
 using mlir::Location;
+using mlir::BlockArgument;
 
 
 void Block_addArgument(Block &block, const Type& type, const Location& loc);
+std::unique_ptr<BlockArgument> Block_getArgument(Block &block, unsigned i);
 
 } // namespace mithril_oxide_sys
