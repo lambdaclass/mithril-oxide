e8296d488779d5bed0431d4bf8cd641c2859aaca
Esteve Soler Arderiu <esteve.soler@lambdaclass.com>
2023-06-16 14:52:24 +0200

    Add `ml_program` types.

---
+++ crates/mithril-oxide-sys/cpp/Dialect/MLProgram/IR/MLProgramTypes.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <mlir/Dialect/MLProgram/IR/MLProgramTypes.h>
+#include <mlir/IR/MLIRContext.h>
+
+
+namespace mithril_oxide_sys::ml_program {
+
+using mlir::ml_program::TokenType;
+using mlir::MLIRContext;
+
+
+const void *TokenType_get(MLIRContext *ctx);
+
+} // mithril_oxide_sys::ml_program
