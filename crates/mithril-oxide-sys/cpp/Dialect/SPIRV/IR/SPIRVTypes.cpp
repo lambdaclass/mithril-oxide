022b2c97bede8c937078988290380bea424ecc31
Esteve Soler Arderiu <esteve.soler@lambdaclass.com>
2023-06-16 17:05:30 +0200

    Add `SPIRV` type placeholders.

---
+++ crates/mithril-oxide-sys/cpp/Dialect/SPIRV/IR/SPIRVTypes.cpp
@@ -0,0 +1,10 @@
+#include "SPIRVTypes.hpp"
+
+#include <mlir/Dialect/SPIRV/IR/SPIRVTypes.h>
+
+
+namespace mithril_oxide_sys::spirv {
+
+// TODO: Getters for SPIR-V types.
+
+} // namespace mithril_oxide_sys::spirv
---
+++ crates/mithril-oxide-sys/cpp/Dialect/SPIRV/IR/SPIRVTypes.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <mlir/Dialect/SPIRV/IR/SPIRVTypes.h>
+
+
+namespace mithril_oxide_sys::spirv {
+
+using mlir::spirv::ArrayType;
+using mlir::spirv::ImageType;
+using mlir::spirv::PointerType;
+using mlir::spirv::RuntimeArrayType;
+using mlir::spirv::SampledImageType;
+using mlir::spirv::StructType;
+
+
+// TODO: Getters for SPIR-V types.
+
+} // namespace mithril_oxide_sys::spirv
