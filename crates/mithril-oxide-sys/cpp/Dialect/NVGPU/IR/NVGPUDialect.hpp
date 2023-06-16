06504b5b97e0ac3c6bdb6d6726cf5550ea6b0ded
Esteve Soler Arderiu <esteve.soler@lambdaclass.com>
2023-06-16 15:17:42 +0200

    Add `nvgpu` types.

---
+++ crates/mithril-oxide-sys/cpp/Dialect/NVGPU/IR/NVGPUDialect.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <mlir/Dialect/NVGPU/IR/NVGPUDialect.h>
+#include <mlir/IR/MLIRContext.h>
+
+
+namespace mithril_oxide_sys::nvgpu {
+
+using mlir::MLIRContext;
+using mlir::nvgpu::DeviceAsyncTokenType;
+
+
+const void *DeviceAsyncTokenType_get(MLIRContext *ctx);
+
+} // namespace mithril_oxide_sys::nvgpu
