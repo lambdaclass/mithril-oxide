0f26526dd6e972ba1175981b5da150049008a102
Esteve Soler Arderiu <esteve.soler@lambdaclass.com>
2023-06-16 16:55:10 +0200

    Implement `sparse_tensor` types.

---
+++ crates/mithril-oxide-sys/cpp/Dialect/SparseTensor/IR/SparseTensor.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <mlir/Dialect/SparseTensor/IR/SparseTensor.h>
+
+
+namespace mithril_oxide_sys::sparse_tensor {
+
+using mlir::sparse_tensor::StorageSpecifierType;
+
+
+const void *StorageSpecifierType_get(const void *encoding);
+
+} // namespace mithril_oxide_sys::sparse_tensor
